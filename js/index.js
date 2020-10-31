968128ced03147d6194a4f69a54d6ee58ac38fc7
diego martinez <juandiego337@hotmail.com>
2020-10-30 23:49:15 -0500

    add scrollreveal in the project

---
+++ js/index.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.header__img', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.scroll-main', {
+    duration: 3000
+});
+
+sr.reveal('.scroll-product-detail', {
+    duration: 3000
+});
+
+sr.reveal('.scroll-image', {
+    duration: 3000
+});
